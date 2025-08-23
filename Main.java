e8b014c5581ff7f20106f1e06c96c5fa151750ca
iya <sofiagache.scc@gmail.com>
2025-08-23 13:41:34 +0000

    link

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("SEATWORKSIX");
+        int dice = 1;
 
+        while (dice <= 6) {
+            if (dice < 6) {
+                System.out.println("No Yatzy.");
+            } else {
+                System.out.println("Yatzy!");
+            }
+            dice = dice + 1;
+        }
+    }
+}
---
+++ Main1.java
@@ -0,0 +1,13 @@
+public class Main1 {
+  public static void main(String[] args) {
+        System.out.println("SEATWORKFIVE");
+
+        int countdown = 3;
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+    
+        System.out.println("Happy New Year!!");
+        }
+    }
+}  
