f5bd03244aba90d81b2cd1c00770645fedc26e03
iya <sofiagache.scc@gmail.com>
2025-09-09 10:09:16 +0000

    java

---
+++ Seatworkeleven.java
@@ -0,0 +1,9 @@
+public class Seatworkeleven {
+    public static void main(String[] args) {
+        System.out.println("Seatworkelevenpart1");
+        int[][] myNumbers = {{1,4,2}, {3,6,8} };
+        myNumbers[1][2]=9;
+
+        System.out.println(myNumbers[1][2]);
+    }
+}
